f8f066756fc659e5d226977aff2f5255241472ab
Andrei Nagacevschi <andrei.nagacevschi@osf-commerce.com>
2018-05-04 19:51:14 +0300

    Firing up the rustypress server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate rustypress;
+
 fn main() {
-    
+    let socket = "3000";
+    let ip = "127.0.0.1";
+    let address = format!("{}:{}", ip, socket);
+
+    println!("server running at localhost:{0}", socket);
+    rustypress::RustyServer::new().http(&address);
 }
